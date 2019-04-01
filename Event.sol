429245258ab085423dd1543ec36eee0d2537104e
Николай Санников <kolyambosunny@gmail.com>
2019-04-01 01:06:28 -0600

    Implemented basic smart contract functionality

---
+++ Event.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.2;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+import './Tickets.sol';
+
+contract Event{
+    string public eventName;
+    
+    bool public released = false;
+    modifier eventReleased() {
+        require(released == true,"Event has not yet been released");
+        _;
+    }
+    modifier isOrganizer() {
+        require(msg.sender == organizer,"Only event organizer allowed to do that");
+        _;
+    }
+    modifier ticketOnSale(uint256 ticketId) {
+        require(ticketIsOnSale(ticketId) == true,"Ticket is not on sale.");
+        _;
+    }
+    
+    Tickets public ticketStorage;
+    mapping(uint256 => uint) TicketPrices;
+    
+    address public organizer;
+    mapping(address => uint) Balances;
+    
+    constructor (string memory _eventName) public{
+        eventName = _eventName;
+        organizer = msg.sender;
+    }
+    function linkTicketPool(address _ticketStorage) isOrganizer public{
+        require(address(ticketStorage)==address(0), "Ticket storage was already linked.");
+        ticketStorage = Tickets(_ticketStorage);
+    }
+    
+    function release() isOrganizer public {
+        require(released == false,"Event has already been released.");
+        released = true;
+    }
+    
+    
+    function sellTicket(uint256 ticketId, uint price) eventReleased public{
+        require(ticketIsOnSale(ticketId),"Contract does not have rights to sell a ticket");
+        TicketPrices[ticketId] = price;
+    }
+    
+    function ticketIsOnSale(uint256 ticketId) view public returns(bool){
+        Tickets tickets = Tickets(ticketStorage);
+        return tickets.getApproved(ticketId)==address(this);
+    }
+    
+    function buyTicket(uint256 ticketId) payable eventReleased ticketOnSale(ticketId) public{
+        require( TicketPrices[ticketId] <= msg.value, "Not enough money.");
+        //make the ticket owner address payable
+        Tickets tickets = Tickets(ticketStorage);
+        address seller = tickets.ownerOf(ticketId);
+        Balances[seller] = msg.value;
+        tickets.safeTransferFrom(seller, msg.sender, ticketId);
+    }
+    
+    function getMyBalance(uint256 ticketId) public returns(uint){
+        return Balances[msg.sender];
+    }
+    
+    function withdraw() public {
+        uint toTransfer = Balances[msg.sender];
+        Balances[msg.sender] =0;
+        msg.sender.transfer(toTransfer);
+    }
+    
+}
+
---
+++ Tickets.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.5.2;
+import 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+import './Event.sol';
+
+contract Tickets is ERC721{
+    //description can be a row-seat combination or anything else
+    struct Ticket{
+        string description;
+        address redeemedBy;
+    }
+    
+    Event public ParentEvent;
+    
+    uint public numberOfTickets=0;
+    uint256[] public TicketIds;
+    mapping( uint256 => Ticket) public TicketData;
+    uint256[] public RedeemedTickets;
+    
+    constructor(Event _eventAddress) public{
+        ParentEvent = _eventAddress;
+    }
+    
+    function addTicket(string memory _description) public{
+        require(ParentEvent.released() == false,"Unable to add any tickets once the event is published");
+        require(msg.sender == ParentEvent.organizer(), "Only event organizer may add tickets.");
+        //require(msg.sender == address(ParentEvent), "Tokens can not be created from an external entity");
+        uint256 ticketId = uint256(keccak256(abi.encode(ParentEvent,_description)));
+        
+        TicketData[ticketId] = Ticket(_description, address(0));
+        TicketIds.push(ticketId);
+        _mint(address(ParentEvent.organizer()), ticketId);
+        numberOfTickets += 1;
+    }
+    
+    function publishOnMarket(uint256 ticketId, uint price) public{
+        require (ownerOf(ticketId) == msg.sender, "Only the owner can send a ticket");
+        require (isRedeemed(ticketId) == false, "Used ticket cannot be sold");
+        approve(address(ParentEvent), ticketId);
+        ParentEvent.sellTicket(ticketId, price);
+    }
+    
+    function cancelMarketPosition(uint256 ticketId) public{
+        require (ownerOf(ticketId) == msg.sender, "Only the owner can send a ticket");
+        approve(address(0), ticketId);
+    }
+    
+    function isRedeemed(uint256 ticketId) public returns(bool){
+        return TicketData[ticketId].redeemedBy != address(0);
+    }
+    
+    function redeem(uint256 ticketId) public {
+        require(msg.sender == ownerOf(ticketId), "Only ticket owner can redeem the ticket");
+        require(getApproved(ticketId) == address(0), "Ticket should not be published on market");
+        
+        require (isRedeemed(ticketId) == false, "Same ticket can not be used twice");
+        safeTransferFrom(msg.sender, address(ParentEvent), ticketId);
+        TicketData[ticketId].redeemedBy = msg.sender;
+        RedeemedTickets.push(ticketId);
+    }
+}
