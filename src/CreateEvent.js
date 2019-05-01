4c9ab9e89c26dde20a0025ed9d015db7843dce2c
Nikolai Sannikov <kolyambosunny@gmail.com>
2019-04-30 22:35:30 -0600

    Deploying contracts on organizer's demand

--- src/CreateEvent.js
+++ src/CreateEvent.js
@@ -1,28 +1,57 @@
 import React, { Component } from "react";
 import { Input, Container, Button } from 'semantic-ui-react';
-import web3Handler from 'web3';
+import web3Handler from './web3';
+import EventData from './EventData'
 
 class CreateEvent extends Component {
   state = {
     eventName: "",
-    message: ""
+    status: "",
+    eventContractAddress: "",
+    ticketContractAddress: "",
+
   };
   
-  deploy = async (compiledContract, eventName) => {
+  deploy = async (eventName) => {
+    const accounts = await web3Handler.eth.getAccounts();     
     
-    const accounts = await web3.eth.getAccounts();     
-    
-    const options = {      
-      transactionConfirmationBlocks: 2,
-    }
+    //deploying event contract
+    const compiledEvent = require("./build/contracts/Event.json");
+    const EventContract = new web3Handler.eth.Contract(compiledEvent.abi);
 
-    const result = await new web3Handler.eth.Contract(
-      compiledContract.abi
-    )
-      .deploy({ data: compiledContract.bytecode, arguments:[eventName] })
-      .send({ gas: "2000000", from: accounts[0] });
-          
-    console.log("Contract deployed at: ", result.options.address);    
+    const eventResult = await EventContract.deploy({ data: compiledEvent.bytecode, arguments:[eventName] })
+      .send({ gas: "6721974", from: accounts[0] });
+
+    console.log("Deployed event contract at: "+eventResult.options.address)
+    this.setState({eventContractAddress: eventResult.options.address})
+
+    //deploying ticket pool
+    const compiledTickets = require("./build/contracts/Tickets.json")
+    const TicketContract = new web3Handler.eth.Contract(compiledTickets.abi);
+
+    const ticketResult = await TicketContract.deploy({ data: compiledTickets.bytecode, arguments:[this.state.eventContractAddress]})
+      .send({ gas: "6721974", from: accounts[0] }); 
+
+    console.log("Deployed ticket pool contract at: "+ticketResult.options.address)
+    this.setState({ticketContractAddress: ticketResult.options.address})
+
+    //link ticket pool to event
+    const EventInstance = await new web3Handler.eth.Contract(compiledEvent.abi,this.state.eventContractAddress)
+    await EventInstance.methods.linkTicketPool(this.state.ticketContractAddress)
+      .send({gas:"6721974", from: accounts[0]})
+
+    const linkedPool = await EventInstance.methods.ticketStorage().call()
+    console.log("Event was linked to: "+linkedPool)
+    const assert = require('assert')
+    if (linkedPool === this.state.ticketContractAddress){
+      console.log("Ticket pool successfully linked")
+      this.setState({status:"success"})
+    }
+    else{
+      console.log("Linked pool result: "+linkedPool)
+      console.log("Ticket pool was not linked: "+linkedPool+" VS "+this.state.ticketContractAddress)
+      this.setState({status:"failed"})
+    }
   };
   
   inputTextChanged = (e) => {
@@ -31,9 +60,8 @@ class CreateEvent extends Component {
     });
   };
   createEventContract = () => {    
-    const compiledEvent = require("./build/contracts/Event.json");   
-    //const compiledTickets = require("./build/contracts/Tickets.json")
-    deploy(compiledEvent,this.state.eventName);
+    this.deploy(this.state.eventName);
+    this.setState({status:"creating"})
   };
 
   
@@ -41,14 +69,25 @@ class CreateEvent extends Component {
   
 
   render() {
+    if(this.state.eventContractAddress!=="")
     return (
       <Container>
         <h1>Create your own event</h1><br/>
         <Input focus onChange={this.inputTextChanged.bind(this)} placeholder='Event name:' />
+        <br/>
+        <Button color='green' onClick={this.createEventContract} >Create</Button>
+      </Container>
+    );
+    else
+    return (
+      <Container>
+        <h1>Create your own event</h1><br/>
+        <Input focus onChange={this.inputTextChanged.bind(this)} placeholder='Event name:' />
+        <br/>
         <Button color='green' onClick={this.createEventContract} >Create</Button>
       </Container>
     );
   }
 }
-
+//        <EventData eventContractAddress={this.state.eventContractAddress}/> : null }
 export default CreateEvent;
